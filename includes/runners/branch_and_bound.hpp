e6b93781fdbb82461da68a3f55c81a6aa4d79c43
Your Name <you@example.com>
2018-08-11 14:29:41 +0200

    update

--- includes/runners/branch_and_bound.hpp
+++ includes/runners/branch_and_bound.hpp
@@ -183,7 +183,8 @@ std::vector<LBSolutionPath<N>> Astar_runner(
 
                             const double my_iteration_time = MPI_Wtime() - __start;
 
-                            std::tie(complexity, received, sent) = {0,0,0}; //computation_info;
+                            //std::tie(complexity, received, sent) = computation_info;
+                            complexity = 0; received = 0; sent = 0;
 
                             MPI_Allgather(&my_iteration_time, 1, MPI_DOUBLE, &times.front(), 1, MPI_DOUBLE, comm);
                             true_iteration_time = *std::max_element(times.begin(), times.end());
